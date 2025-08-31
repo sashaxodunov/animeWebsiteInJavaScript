e1269c6008b5675e74e5dcdaf9ca909eaaccb71b
sashaxodynov <103351359+sashaxodunov@users.noreply.github.com>
2025-08-31 10:32:41 +0300

    Урок1.1

--- js/search.js
+++ js/search.js
@@ -1,6 +1,16 @@
-const search = modal.querySelector('#search-input')
+const searchInput = modal.querySelector('#search-input')
+const modalInput = document.querySelector('.search-model')
 
-console.log(search)
-search.addEventListener('input', (e) => {
-  console.log(e.target.value)
+// search.addEventListener('input', (e) => {
+//   console.log(e.target.value)
+// })
+
+searchInput.addEventListener('keydown', (e) => {
+  if (e.key === 'Enter') {
+    console.log(e.target.value)
+    modalInput.style.display = 'none'
+    searchInput.value = ''
+  }
 })
+
+console.dir(searchInput)
